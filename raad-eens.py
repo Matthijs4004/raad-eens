1e667fb89aa8e56569aef1d5c93cc7545b531bc7
Matthijs Raatgever <matthijs4004@gmail.com>
2021-09-22 09:27:35 +0200

    raad eens

---
+++ raad-eens.py
@@ -0,0 +1,35 @@
+import random
+
+laag = 1
+hoog = 1000
+round = 0
+score = 0
+
+# loop waarmee je alle 20 rondes uitvoert
+while round < 20:
+    round += 1
+    count = 0
+    num = random.randint(laag, hoog)
+    
+    print("Ronde ", round)
+    if round < 19:
+        print("Nog een getal raden?")
+
+    while count < 10:
+	    count += 1
+        
+
+        # input voor number waarmee je het getal moet raden
+	    gok = int(input("Gok een nummer: "))
+		
+	    if num == gok:
+		    print("Goed gedaan! Je hebt ",count, " gokken gedaan voordat je het goed had. Score: ", score)
+	    elif num > gok:
+		    print("Je hebt te klein gegokt!")
+	    elif num < gok:
+		    print("Je hebt te hoog gegokt!")
+
+    # Als je meer hebt gegokt dan het aantal gokken dan komt er deze output te staan
+    if count >= 10:
+	    print("Het nummer was", num)
+	    print("Veel geluk voor de volgende keer!")
